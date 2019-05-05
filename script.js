1b3d8f153d0a685481155dfaa50f49f99ccceb7c
mryolo9123 <48654776+mryolo9123@users.noreply.github.com>
2019-05-05 10:55:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function buton(){
-  document.getElementById("lol").textcontent = "ldsqwfejkhbvt r"
+function jjj() {
+  document.getElementById("lol").innerHTML = "mÆde using c+++++ scripts (not fake!)•¶¡™§˜∫çßå∆˚çƒ¨˚œ∑ç¨çå≤ßµ∫ßå¬… ¥ˆ¨†¡™..."
+}
+function kasretf() {
+  var x  = document.getElementsByTagName("body")[0];
+  x.style.background = 'url("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")'
 }
