fbe32a072912c222a705e936996565d24849a866
mryolo9123 <48654776+mryolo9123@users.noreply.github.com>
2019-05-19 09:31:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function kasretf() {
   var x  = document.getElementsByTagName("body")[0];
   x.style.background = 'url("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")'
 }
+function thebruh() {
+  document.getElementById("kjkj").innerHTML = "OMG A TORNADO~!!"
+}
+function lolkkk() {
+  document.getElementById("ggg").innerHTML = "idk!?"
+}
+function ogstate() {
+  document.getElementById("lol").innerHTML = "mÆde using .lua scripts (not fake!)•¶¡™§˜∫çßå∆˚çƒ¨˚œ∑ç¨çå≤ßµ∫ßå¬… ¥ˆ¨†¡™..."
+  var x  = document.getElementsByTagName("body")[0];
+  x.style.background = 'url("http://fornaxvoid.com/colorpalettes/AmstradCPC-palette.png")'
+  document.getElementById("kjkj").innerHTML = "bruhrubrurbhb"
+  document.getElementById("ggg").innerHTML = ""
+  
+}
+function crash() {
+  document.getElementById("NOOO").innerHTML = "please wait..."
+  location.href = "chrome://crash";
+}
